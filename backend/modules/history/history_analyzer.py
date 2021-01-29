6013671fdda2a975e930ec17f8d7cd5ce5231219
AgusNeira <a2002neira@gmail.com>
2021-01-29 13:56:13 -0300

    [MOD] historyAnalyzer.py is now the module's main file

--- backend/modules/history/history_analyzer.py
+++ backend/modules/history/history_analyzer.py
@@ -1,54 +1,19 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from browser_history import get_history
-from browser_history import browsers
-
-import summarize
-
-# Gathers the histories from all available browsers
+## History analizer
 #
-# The object returned is of type <browser_history.generic.Outputs>
-# The histories in [(datetime, url)] format can be accessed through outputs.histories
-# Some useful methods of the returned object are to_json() and to_csv(), both returning strings
+# This is the main file for this module, where the test cases should be developed and where
+# the functions called by the API must be located it depends on several other files, mainly:
 #
-# One thing to note is that the data obtained from this method deosn't distinguish between
-# browsers, meaning you can't find out which entry belongs to which browser
+# fetcher.py    - Uses the browser_history library to actually obtains the histories
+# summarize.py  - Collects the data to analyze repetitions and patterns. Its outputs may easily be represented in a histogram
 #
-def fetch_all():
-    return get_history()
+# Other files may be added as needed while the development continues
 
-# Gathers information from specific browsers
-#
-# The browsers to be analyzed are specified in the first argument, which is an array of strings
-# The output format is { browser_name: <browser_history.generic.Outputs> }; you can check
-# pesos/browser_history's documentation for details of the Outputs object
-#
-def fetch_specific(args):
-    dict_browsers = {
-            'Brave': browsers.Brave,
-            'Chrome': browsers.Chrome,
-            'Chromium': browsers.Chromium,
-            'Edge': browsers.Edge,
-            'Firefox': browsers.Firefox,
-            'Opera': browsers.Opera,
-            'OperaGX': browsers.OperaGX,
-            'Safari': browsers.Safari
-    }
-
-    output = {}
-
-    for browser_name in args:
-        if browser_name in dict_browsers:
-            try:
-                brows_init = dict_browsers[browser_name]()
-            
-                output[browser_name] = brows_init.fetch_history()
-            except AssertionError as err:
-                print('INFO: browser {} not supported'.format(err))
-
-    return output
+from fetcher import fetch_all, fetch_specific
 
 if __name__ == '__main__':
-    data = fetch_specific(['Edge'])
+    data = fetch_all()
+
     print(data)
